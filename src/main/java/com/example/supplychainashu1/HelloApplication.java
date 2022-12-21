499a93c66d2b8eb3e31e8383f62f05563271a329
Ashutoshmalav <malavashutosh60@gmail.com>
2022-12-21 13:25:48 +0530

    Login Button

--- src/main/java/com/example/supplychainashu1/HelloApplication.java
+++ src/main/java/com/example/supplychainashu1/HelloApplication.java
@@ -1,6 +1,8 @@
 package com.example.supplychainashu1;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -49,6 +51,18 @@ public class HelloApplication extends Application {
 
         TextField emailTextField =  new TextField();
         PasswordField passwordField = new PasswordField();
+        Label messageLabel =new Label("I am message");
+
+        Button loginButton = new Button("Login");
+
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String email = emailTextField.getText();
+                String password = passwordField.getText();
+                messageLabel.setText(email +" @@ " + password);
+            }
+        });
 
         GridPane gridPane = new GridPane();
         gridPane.setMinSize(bodyPane.getMinWidth(),bodyPane.getMinHeight());
@@ -63,7 +77,8 @@ public class HelloApplication extends Application {
         gridPane.add(emailTextField,1,0);
         gridPane.add(passwordLabel,0,1);
         gridPane.add(passwordField,1,1);
-
+        gridPane.add(loginButton,0,2);
+        gridPane.add(messageLabel,1,2);
         return gridPane;
     }
     private Pane createContent(){
